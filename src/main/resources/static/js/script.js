72b57d674da23639807edb93e8924ddec8322cd5
Galagyy <109639693+Galagyy@users.noreply.github.com>
2024-08-16 22:35:37 -0700

    Adding Comments
    Added in comments for each process in order to make it easier to read and understand.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,9 +1,19 @@
+// Adding an event listener to the page so that
+// we can listen for events (self explanatory..)
 document.addEventListener("DOMContentLoaded", function () {
+  // Since we made a buttons class, we're using that to retrieve
+  // ALL of the buttons. (that's why its querySelectorALL)
+  //                                                  ^^^
   const buttons = document.querySelectorAll(".image_button");
 
+  // For this section I just turned it into English for simplicity.
+  // For each button...
   buttons.forEach((button) => {
+    // Add an event listener for when the mouse enters its area...
     button.addEventListener("mouseenter", () => {
+      // And create a new sound from our sounds folder...
       const hoverSound = new Audio("sounds/Hover Sound.mp3")
+      // And play it.
       hoverSound.play();
     });
   });
